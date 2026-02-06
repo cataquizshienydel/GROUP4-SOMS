6af0efdcbdc08e64067169335c48caa69cb17aea
cataquizshienydel <cataquizshienydel@gmail.com>
2026-02-06 19:33:03 +0800

    first commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,7 @@
+import { Routes } from '@angular/router';
+import { LoginComponent } from './components/auth/login/login';
+export const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  
+];
